7338f466db06c32e550ff04500094da120abbabe
Arslan01yaqoob <151700033+Arslan01yaqoob@users.noreply.github.com>
2023-12-12 10:17:16 +0500

    Add files via upload

---
+++ sing-up.js
@@ -0,0 +1,13 @@
+// button
+
+
+
+$(document).ready(function () {
+    $('#btn').fadeIn(2000);
+});
+
+// container slide
+$('#box').ready(function () {
+
+    $("#box").hide().slideDown(1000); // Adjust the durations as needed
+});
